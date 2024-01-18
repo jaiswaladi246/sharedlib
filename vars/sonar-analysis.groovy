0cfb31245937a491795e0989a1bca81c7b37d137
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2024-01-18 12:52:35 +0530

    Update sonar-analysis.groovy

--- vars/sonar-analysis.groovy
+++ vars/sonar-analysis.groovy
@@ -1,9 +1,22 @@
 
-def call (Project){
-  def sonarCommand = ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.url=http://13.233.67.35:9000 -Dsonar.projectName=${Project}  \
-                   -Dsonar.java.binaries=. \
-                   -Dsonar.projectKey=${Project} '''
+// File: vars/sonarAnalysis.groovy
 
-  sh script: sonarCommand, returnStatus: true
-
+def call() {
+    // Define SonarQube server details
+    def sonarHostUrl = "http://13.233.67.35:9000"
+    def sonarProjectKey = "ABCD"
+    def sonarProjectName = "ABCD"
+    
+    // Define SonarQube scanner properties
+    def sonarScannerHome = tool 'sonar-scanner'
+    def sonarScannerOpts = "-Xmx512m"  // Adjust memory settings as needed
+    
+    // Execute Sonar analysis
+    sh """
+        ${sonarScannerHome}/bin/sonar-scanner \
+        -Dsonar.host.url=${sonarHostUrl} \
+        -Dsonar.projectKey=${sonarProjectKey} \
+        -Dsonar.projectName=${sonarProjectName} \
+        ${sonarScannerOpts}
+    """
 }
