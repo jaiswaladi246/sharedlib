769c7dcd1afe03615b2f894a35d239ff37b9c218
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2024-01-18 12:12:17 +0530

    Update pkg.groovy

--- vars/pkg.groovy
+++ vars/pkg.groovy
@@ -1,3 +1,14 @@
-def pkg() {
-  mvn package
+def call() {
+    stage('Maven Package') {
+        // Define the Maven command to be executed
+        def mavenCommand = 'mvn package'
+
+        // Execute the Maven command in a shell
+        sh script: mavenCommand, returnStatus: true
+
+        // Check the return status of the Maven command
+        if (currentBuild.resultIsBetterOrEqualTo('FAILURE')) {
+            error "Maven build failed"
+        }
+    }
 }
