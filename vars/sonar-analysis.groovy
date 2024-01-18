ecdda7815d725cd5c4d464f155df6c8016532c82
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2024-01-18 12:47:40 +0530

    Update sonar-analysis.groovy

--- vars/sonar-analysis.groovy
+++ vars/sonar-analysis.groovy
@@ -1,6 +1,6 @@
 
 def call (Project){
-  def sonarCommand = ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${Project} \
+  def sonarCommand = ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.url=http://13.233.67.35:9000 -Dsonar.projectName=${Project}  \
                    -Dsonar.java.binaries=. \
                    -Dsonar.projectKey=${Project} '''
 
