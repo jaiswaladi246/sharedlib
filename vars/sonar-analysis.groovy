76ba1d94e82d1406b24e00b2e4220852c106d436
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2024-01-18 13:13:20 +0530

    Update sonar-analysis.groovy

--- vars/sonar-analysis.groovy
+++ vars/sonar-analysis.groovy
@@ -2,6 +2,7 @@
 // File: vars/sonarAnalysis.groovy
 
 def call() {
+   def SCANNER_HOME= tool 'sonar-scanner'
    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=E-Kart \
                    -Dsonar.java.binaries=. \
                    -Dsonar.projectKey=E-Kart '''
